81613c9dccd663e1696473be29190640cb9e2927
EperezLOGALI <enpesan@hotmail.com>
2024-07-08 13:41:46 +0000

    First Commit 05

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,30 +1,96 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      */
-function (Controller) {
-    "use strict";
+    function (Controller, Filter, FilterOperator) {
+        "use strict";
 
-    return Controller.extend("logaligroup.employee.controller.MainView", {
-        onInit: function () {
+        function onInit() {
 
-        },
+            var oJSONModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            var i18nBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle(); //logaligroup.employee.i18n.i18n
 
-        onValidate: function () {
+            // var oJSON = {
+            //     employeeId: "12345",
+            //     countryKey: "UK",
+            //     listCountry: [
+            //         {
+            //             key: 'US',
+            //             text: i18nBundle.getText("countryUS")
+            //         },
+            //         {
+            //             key: 'UK',
+            //             text: i18nBundle.getText("countryUK")
+            //         },
+            //         {
+            //             key: 'ES',
+            //             text: i18nBundle.getText("countryES")
+            //         }
+            //     ]
+            // };
+
+            //oJSONModel.setData(oJSON);
+            oJSONModel.loadData("./localService/mockdata/Employees.json", false);
+            // oJSONModel.attachRequestCompleted( function() {
+            //     console.log(JSON.stringify(oJSONModel.getData()));
+            // });
+            oView.setModel(oJSONModel);
+        }
+
+        function onFilter() {
+
+            var oJSON = this.getView().getModel().getData();
+            var filters = [];
+
+            if (oJSON.EmployeeId !== "") {
+
+                filters.push(new Filter("EmployeeID",FilterOperator.EQ,oJSON.EmployeeId ));
+            }
+            if (oJSON.countryKey !== "") {
+
+                filters.push(new Filter("Country",FilterOperator.EQ,oJSON.countryKey ));
+            }
+
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters);           
+
+        }
+
+        function onClearFilter() {
+
+            var oModel = this.getView().getModel();
+
+            oModel.setProperty("/EmployeeId","");
+            oModel.setProperty("/countryKey","");
+        }
+        var Main = Controller.extend("logaligroup.employee.controller.MainView", {});
+
+        //        return Controller.extend("logaligroup.employee.controller.MainView", {
+
+        Main.prototype.onValidate = function () {
             var inputEmployee = this.byId("inputEmployee");
             var valueEmployee = inputEmployee.getValue();
 
             if (valueEmployee.length === 6) {
                 //inputEmployee.setDescription("OK");
-                this.byId("labelCountry").setVisible(true);
-                this.byId("slCountry").setVisible(true);
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
             } else {
                 //inputEmployee.setDescription("Not OK");
-                this.byId("labelCountry").setVisible(false);
-                this.byId("slCountry").setVisible(false);
-            };
-        }
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            }
+        };
+
+        Main.prototype.onInit = onInit;
+        Main.prototype.onFilter = onFilter;
+        Main.prototype.onClearFilter = onClearFilter
+
+        return Main;
     });
-});
